6cab685413a673ae7aa1d7e27fa3bb2a1a1e5f08
Uwe L. Korn <uwelk@xhochy.com>
2012-01-20 16:23:45 +0100

    [db] Script to start locally a MongoDB instance.

--- db/mongo.sh
+++ db/mongo.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mongod --master --bind_ip 127.0.0.1 --port 27117 --dbpath mongo-store --noprealloc  --unixSocketPrefix mongo-sock --nohttpinterface --cpu
+mongod --master --bind_ip 127.0.0.1 --port 27117 --dbpath mongo-store --noprealloc  --unixSocketPrefix mongo-sock --nohttpinterface
 
